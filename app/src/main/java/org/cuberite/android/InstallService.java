edd631eadfc561d17ff29ef29a2bd5618d115a66
Mat <mail@mathias.is>
2019-11-16 02:41:51 +0200

    Switch Target API to 29 (#15)

--- app/src/main/java/org/cuberite/android/InstallService.java
+++ app/src/main/java/org/cuberite/android/InstallService.java
@@ -6,9 +6,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PowerManager;
-import android.support.v4.content.LocalBroadcastManager;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.support.v4.os.ResultReceiver;
-import android.support.v7.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog;
 import android.util.Log;
 
 import java.io.BufferedOutputStream;
