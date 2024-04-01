8bc1ca211254749a657e5dd6ffccef59d3465d51
LooKeR <mohit2002ss@gmail.com>
2024-04-01 20:09:44 +0530

    Migrate to latest `build.gradle.kts` format
    Move `settings.gradle` -> `settings.gradle.kts`
    Move plugin repositories `settings.gradle.kts`
    Remove unused import for `jar` files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-buildscript {
-    repositories {
-        mavenCentral()
-        google()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.1")
-        classpath(kotlin("gradle-plugin", version = "2.0.0-Beta5"))
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle.kts files
-    }
-}
-
-allprojects {
-    repositories {
-        mavenCentral()
-        google()
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+plugins {
+    id("com.android.application") version "8.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0-Beta5" apply false
 }
