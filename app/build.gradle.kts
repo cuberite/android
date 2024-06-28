320834eeb39e8df01a13d516eb5f2d7f0839f3a4
LooKeR <mohit2002ss@gmail.com>
2024-06-28 19:41:03 +0530

    Update compose dependency and add navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
 
 dependencies {
 
-    val composeBom = platform("androidx.compose:compose-bom:2024.04.00")
+    val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
 
     // Compose
     implementation(composeBom)
@@ -60,6 +60,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
